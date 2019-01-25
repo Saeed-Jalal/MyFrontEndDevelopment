8102fbfa34851ecffc4ecf90a78464074e41a2ae
Muhammad Saeed <syedsaeedjalal@gmail.com>
2019-01-25 21:54:37 +0200

    firstJs

---
+++ myJavaScriptLearning/main.js
@@ -0,0 +1,5 @@
+function wishGreetings(greetings) {
+    var responce = prompt ("Enter Your Name Please...");
+    alert(greetings + " " + responce + ".");
+}
+wishGreetings("We wish you a very great day!");
